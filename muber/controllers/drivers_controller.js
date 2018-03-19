f63de4f0a913e7009fe6558160db251aae8b7ca3
darshit soni <darshitvagadiya@yahoo.com>
2018-03-20 00:54:06 +0530

    initial commit

---
+++ muber/controllers/drivers_controller.js
@@ -0,0 +1,50 @@
+const Driver = require('../models/driver');
+const mongoose = require('mongoose');
+module.exports = {
+	greeting(req, res){
+		res.send({ hi: 'there' });
+	},
+
+	index(req, res, next) {
+	    const {lng, lat} = req.query;
+	 
+	    Driver.find({
+	        'geometry.coordinates': {
+	            $nearSphere: {
+	                $geometry: {
+	                    type: "Point",
+	                    coordinates: [lng, lat]
+	                },
+	                $maxDistance: 200000
+	            }
+	        }
+	    })
+	        .then(drivers => res.send(drivers))
+	        .catch(next);
+	},
+
+	create(req, res, next){
+		const driverProps = req.body;
+		Driver.create(driverProps)
+			.then(driver => res.send(driver))
+			.catch(next);
+	},
+
+	edit(req, res, next){
+		const driverId = req.params.id;
+		const driverProps = req.body;
+
+		Driver.findByIdAndUpdate({ _id: driverId }, driverProps)
+			.then(() => Driver.findById({ _id: driverId }))
+			.then(driver => res.send(driver))
+			.catch(next);
+	},
+
+	delete(req, res, next){
+		const driverId = req.params.id;
+
+		Driver.findByIdAndRemove({ _id: driverId })
+			.then(driver => res.send(driver))
+			.catch(next);
+	}
+};
